dcf998eebf20e97770bc2a697052897c29b34498
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-04-04 09:59:12 +0200

    Implementación funcionalidade desbloquear contido cada día

--- app/src/main/java/com/galegando21/MainActivity.kt
+++ app/src/main/java/com/galegando21/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.galegando21
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.galegando21.databinding.ActivityMainBinding
 import com.galegando21.day01Pasagalego.PasagalegoQuestionActivity
 import com.galegando21.day02Musica.MusicaActivity
+import java.util.Calendar
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,8 +26,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        day01Button = findViewById(R.id.day01Button)
-        day02Button = findViewById(R.id.day02Button)
+        updateButtonState()
+
+        day01Button = findViewById(R.id.btnDay1)
+        day02Button = findViewById(R.id.btnDay2)
 
         // Asegurarse de que el fragmento esté agregado antes de llamar a setBannerText
         bannerFragment = supportFragmentManager.findFragmentById(R.id.bannerFragment) as BannerFragment
@@ -47,8 +53,47 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            unlockAllButtons() // Llama a la función para desbloquear todos los botones
+            Snackbar.make(view, "Botones desbloqueados", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+
+    }
+
+    // Función para verificar y actualizar el estado de los botones
+    private fun updateButtonState() {
+        val currentDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
+        // Inicializar SharedPreferences para almacenar el estado de los botones desbloqueados
+        val sharedPreferences = getSharedPreferences("BotonesDesbloqueados", Context.MODE_PRIVATE)
+        // Obtener último día en el que se desbloqueó un botón
+        val lastDayUnlocked = sharedPreferences.getInt("lastDayUnlocked", -1)
+
+        if (currentDay != lastDayUnlocked) {
+            // Se desbloquea un nuevo botón cada día
+            val unlockedButtonCount = sharedPreferences.getInt("unlockedButtonCount", 0)
+            if (unlockedButtonCount < 21) {
+                sharedPreferences.edit().putInt("unlockedButtonCount", unlockedButtonCount + 1)
+                sharedPreferences.edit().putInt("lastDayUnlocked", currentDay)
+                sharedPreferences.edit().apply()
+            }
+        }
+
+        // Configurar la visibilidad de los botones según su estado de desbloqueo
+        val unlockedButtonCount = sharedPreferences.getInt("unlockedButtonCount", 0)
+        for (i in 1..2) {
+            Log.d("DAY", "btnDay$i")
+            val buttonId = resources.getIdentifier("btnDay$i", "id", packageName)
+            val button = findViewById<Button>(buttonId)
+            button.visibility = if (i <= unlockedButtonCount) View.VISIBLE else View.INVISIBLE
+        }
+    }
+
+    private fun unlockAllButtons() {
+        for (i in 1..2) {
+            Log.d("DAY", "btnDay$i")
+            val buttonId = resources.getIdentifier("btnDay$i", "id", packageName)
+            val button = findViewById<Button>(buttonId)
+            button.visibility = View.VISIBLE
         }
     }
 
