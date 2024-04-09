f72d7aad24b164b6ab25bb3c7d79268efdb3b19e
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-04-09 11:21:26 +0200

    Aforcado - Implementación páxina inicio

---
+++ app/src/main/java/com/galegando21/day05Aforcado/AforcadoInicioActivity.kt
@@ -0,0 +1,42 @@
+package com.galegando21.day05Aforcado
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import com.galegando21.BannerFragment
+import com.galegando21.MainActivity
+import com.galegando21.R
+
+class AforcadoInicioActivity : AppCompatActivity() {
+    private lateinit var bannerFragment: BannerFragment
+    private lateinit var comezarButton : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aforcado_inicio)
+
+        // Settear el banner
+        bannerFragment = supportFragmentManager.findFragmentById(R.id.bannerFragment) as BannerFragment
+        supportFragmentManager.beginTransaction().runOnCommit {
+            bannerFragment.setBannerText(getString(R.string.aforcado))
+        }.commit()
+
+        comezarButton = findViewById(R.id.start_btn_aforcado)
+
+        comezarButton.setOnClickListener {
+            Intent(this@AforcadoInicioActivity, MainActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Intent(this@AforcadoInicioActivity, MainActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        })
+    }
+}
