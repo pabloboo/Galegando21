51a5a8db0b0947e096001f49ceea978d1c62bec4
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-04-18 11:46:51 +0200

    Mostrar estadísticas Atrápame Un Millón e Verdade ou Mentira

--- app/src/main/java/com/galegando21/StatisticsActivity.kt
+++ app/src/main/java/com/galegando21/StatisticsActivity.kt
@@ -12,6 +12,8 @@ class StatisticsActivity : AppCompatActivity() {
     private lateinit var timePasagalego : TextView
 
     private lateinit var questionsNeededAtrapameSePodes : TextView
+    private lateinit var cashAtrapaUnMillon : TextView
+    private lateinit var verdadeOuMentiraScore : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +26,21 @@ class StatisticsActivity : AppCompatActivity() {
         errorAnswersPasagalego = findViewById(R.id.error_answers_pasagalego_statistics)
         timePasagalego = findViewById(R.id.time_pasagalego_statistics)
         questionsNeededAtrapameSePodes = findViewById(R.id.questions_needed_atrapame_se_podes_statistics)
+        cashAtrapaUnMillon = findViewById(R.id.cash_atrapa_un_millon_statistics)
+        verdadeOuMentiraScore = findViewById(R.id.verdade_ou_mentira_correct_answers_statistics)
 
         val sharedPreferences = getSharedPreferences("statistics", MODE_PRIVATE)
         correctAnswersPasagalego.text = sharedPreferences.getInt("pasagalego_correct_answers", 0).toString()
         errorAnswersPasagalego.text = sharedPreferences.getInt("pasagalego_error_answers", 0).toString()
         timePasagalego.text = sharedPreferences.getString("pasagalego_time", "00:00")
+
         val questionsNeededAtrapameSePodesValue = sharedPreferences.getInt("atrapame_se_podes_questions_needed", 0).toString()
         questionsNeededAtrapameSePodes.text = "Necesitaches un total de $questionsNeededAtrapameSePodesValue preguntas."
+
+        val cashAtrapaUnMillonValue = sharedPreferences.getInt("atrapa_un_millon_max_cash", 0).toString()
+        cashAtrapaUnMillon.text = "$cashAtrapaUnMillonValue€"
+
+        verdadeOuMentiraScore.text = sharedPreferences.getInt("verdade_ou_mentira_max_score", 0).toString()
 
     }
 }
