deb63ca0c152fc04e7b9d5e598ffd4a384487633
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-04-18 11:19:40 +0200

    Mostrar estadísticas Atrápame Se Podes

--- app/src/main/java/com/galegando21/StatisticsActivity.kt
+++ app/src/main/java/com/galegando21/StatisticsActivity.kt
@@ -11,6 +11,8 @@ class StatisticsActivity : AppCompatActivity() {
     private lateinit var errorAnswersPasagalego : TextView
     private lateinit var timePasagalego : TextView
 
+    private lateinit var questionsNeededAtrapameSePodes : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
@@ -21,11 +23,14 @@ class StatisticsActivity : AppCompatActivity() {
         correctAnswersPasagalego = findViewById(R.id.correct_answers_pasagalego_statistics)
         errorAnswersPasagalego = findViewById(R.id.error_answers_pasagalego_statistics)
         timePasagalego = findViewById(R.id.time_pasagalego_statistics)
+        questionsNeededAtrapameSePodes = findViewById(R.id.questions_needed_atrapame_se_podes_statistics)
 
         val sharedPreferences = getSharedPreferences("statistics", MODE_PRIVATE)
         correctAnswersPasagalego.text = sharedPreferences.getInt("pasagalego_correct_answers", 0).toString()
         errorAnswersPasagalego.text = sharedPreferences.getInt("pasagalego_error_answers", 0).toString()
         timePasagalego.text = sharedPreferences.getString("pasagalego_time", "00:00")
+        val questionsNeededAtrapameSePodesValue = sharedPreferences.getInt("atrapame_se_podes_questions_needed", 0).toString()
+        questionsNeededAtrapameSePodes.text = "Necesitaches un total de $questionsNeededAtrapameSePodesValue preguntas."
 
     }
 }
--- app/src/main/java/com/galegando21/day03AtrapameSePodes/AtrapameSePodesResultActivity.kt
+++ app/src/main/java/com/galegando21/day03AtrapameSePodes/AtrapameSePodesResultActivity.kt
@@ -3,6 +3,7 @@ package com.galegando21.day03AtrapameSePodes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.galegando21.MainActivity
@@ -15,6 +16,8 @@ class AtrapameSePodesResultActivity : AppCompatActivity() {
     private lateinit var stepsFragment: AtrapameSePodesStepsFragment
     private lateinit var atrapameSePodesResultTV: TextView
     private lateinit var atrapameSePodesFinishButton: Button
+
+    private var score = 999999999
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_atrapame_se_podes_result)
@@ -30,7 +33,7 @@ class AtrapameSePodesResultActivity : AppCompatActivity() {
             stepsFragment.setStepsImage(5)
         }.commit()
 
-        val score = intent.getIntExtra(AtrapameSePodesConstants.SCORE, 0)
+        score = intent.getIntExtra(AtrapameSePodesConstants.SCORE, 999999999)
         atrapameSePodesResultTV.text = "Necesitaches un total de $score preguntas para subir 5 escalóns."
 
         atrapameSePodesFinishButton.setOnClickListener {
@@ -40,6 +43,26 @@ class AtrapameSePodesResultActivity : AppCompatActivity() {
             }
         }
 
+        changeAtrapameSePodesStatistics()
+
         setOnBackPressed(this, AtrapameSePodesInicioActivity::class.java)
+    }
+
+    private fun changeAtrapameSePodesStatistics() {
+        val sharedPreferences = getSharedPreferences("statistics", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        var minQuestionsNeeded = 999999999
+        if (sharedPreferences.contains("atrapame_se_podes_questions_needed")) {
+            minQuestionsNeeded = sharedPreferences.getInt("atrapame_se_podes_questions_needed", 999999999)
+        }
+
+        var currentQuestionsNeeded = score
+        if (currentQuestionsNeeded < minQuestionsNeeded) {
+            editor.putInt("atrapame_se_podes_questions_needed", currentQuestionsNeeded)
+            editor.apply()
+        }
+        Log.d("AtrapameSePodesResultActivity", "Preguntas necesarias: ${sharedPreferences.getInt("atrapame_se_podes_questions", 999999999)}")
+
     }
 }
