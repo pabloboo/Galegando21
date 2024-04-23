3fbb7829757f12b7ea5c897172fb3d9eae8ce346
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-04-23 12:05:47 +0200

    Ruleta da Sorte - Implementación funcionalidade de tirar a ruleta

--- app/src/main/java/com/galegando21/day13RuletaDaSorte/RuletaDaSorteGameActivity.kt
+++ app/src/main/java/com/galegando21/day13RuletaDaSorte/RuletaDaSorteGameActivity.kt
@@ -1,10 +1,14 @@
 package com.galegando21.day13RuletaDaSorte
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -17,22 +21,31 @@ import com.galegando21.utils.QuestionRuletaDaSorteConstants
 import com.galegando21.utils.QuestionRuletaDaSorteConstants.MAX_CHARS_PER_LINE
 import com.galegando21.utils.setBanner
 import com.galegando21.utils.setOnBackPressed
+import kotlin.random.Random
 
 class RuletaDaSorteGameActivity : AppCompatActivity() {
     private lateinit var boardLayout: LinearLayout
     private lateinit var hintTv: TextView
+    private lateinit var tirarRuletaButton: Button
+    private lateinit var ruletaAccionTextView: TextView
+    private lateinit var cashTv: TextView
     private lateinit var lettersLayout: ConstraintLayout
 
     private lateinit var questionList: List<QuestionRuletaDaSorte>
     private lateinit var question: QuestionRuletaDaSorte
     private var letterViews: List<TextView> = listOf()
     private var specialChars = listOf(',')
+    private var nextMultiplicadorAccion = 0 // Multiplicador de la acción de la ruleta
+    private var cash = 0 // Dinero acumulado
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ruleta_da_sorte_game)
 
         boardLayout = findViewById(R.id.ruleta_sorte_board_ll)
         hintTv = findViewById(R.id.ruleta_sorte_hint_tv)
+        tirarRuletaButton = findViewById(R.id.ruleta_sorte_button)
+        ruletaAccionTextView = findViewById(R.id.ruleta_sorte_accion_ruleta_tv)
+        cashTv = findViewById(R.id.ruleta_sorte_cash_tv)
         lettersLayout = findViewById(R.id.ruletaDaSorteLettersLayout)
 
         questionList = QuestionRuletaDaSorteConstants.getQuestions()
@@ -82,7 +95,155 @@ class RuletaDaSorteGameActivity : AppCompatActivity() {
         }
 
         hintTv.text = question.hint
+        cashTv.text = "$cash€"
 
+        //La ruleta aún no ha sido tirada
+        setVisibility(false)
+        tirarRuletaButton.setOnClickListener {
+            tirarRuleta()
+        }
+    }
+
+    private fun playLetter(letterGuessed: Char) {
+        var numAciertos = 0
+        // Mapa de vocales sin tilde a vocales con tilde
+        val accentMap = mapOf(
+            'A' to listOf('Á'),
+            'E' to listOf('É'),
+            'I' to listOf('Í'),
+            'O' to listOf('Ó'),
+            'U' to listOf('Ú'),
+        )
+
+        // Revelar la letra jugada
+        for (letter in letterViews) {
+            if (letter.text == letterGuessed.toString()) {
+                letter.setTextColor(ContextCompat.getColor(this@RuletaDaSorteGameActivity, R.color.black))
+                numAciertos++
+            }
+
+            // Si la letra jugada es una vocal, revelar también las vocales con tilde correspondientes
+            if (accentMap.containsKey(letterGuessed)) {
+                for (accentedLetter in accentMap[letterGuessed]!!) {
+                    if (letter.text == accentedLetter.toString()) {
+                        letter.setTextColor(ContextCompat.getColor(this@RuletaDaSorteGameActivity, R.color.black))
+                        numAciertos++
+                    }
+                }
+            }
+        }
+
+        // Actualizar el dinero acumulado
+        cash += numAciertos * nextMultiplicadorAccion
+        cashTv.text = "$cash€"
+        // La letra ya ha sido jugada -> tirar ruleta
+        setVisibility(false)
+    }
+
+    private fun isGameWon(): Boolean {
+        // Verificar si el juego ha sido ganado
+        for (letter in letterViews) {
+            if (letter.currentTextColor == ContextCompat.getColor(this@RuletaDaSorteGameActivity, R.color.canela)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun tirarRuleta() {
+        playRuleta()
+        setVisibility(true)
+    }
+
+    private fun playRuleta() {
+        // Tirar la ruleta
+        val acciones = listOf<String>("0", "25", "50", "75", "100", "150", "bote", "quiebra")
+        val random = Random.nextInt(1, 100)
+        val accion = when {
+            random <= 5 -> acciones[0]
+            random <= 15 -> acciones[1]
+            random <= 40 -> acciones[2]
+            random <= 75 -> acciones[3]
+            random <= 94 -> acciones[4]
+            random <= 96 -> acciones[5]
+            random <= 100 -> acciones[6]
+            else -> acciones[0]
+        }
+
+        // ANIMACIÓN DE TIRAR LA RULETA
+        // Crear el ValueAnimator
+        val animator = ValueAnimator.ofInt(0, acciones.size * 3)
+        animator.duration = 2000
+        // Actualizar el texto de ruletaAccionTextView en cada actualización de la animación
+        animator.addUpdateListener { animation ->
+            val currentIndex = animation.animatedValue as Int
+            ruletaAccionTextView.text = acciones[currentIndex % acciones.size]
+        }
+        // Cuando la animación termine, establecer el texto final
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                playRuletaAccion(accion)
+            }
+        })
+        // Iniciar la animación
+        animator.start()
+    }
+
+    private fun playRuletaAccion(accion: String) {
+        // Realizar la acción de la ruleta
+        when (accion) {
+            "0" -> {
+                ruletaAccionTextView.text = "0"
+                nextMultiplicadorAccion = 0
+            }
+            "25" -> {
+                ruletaAccionTextView.text = "25"
+                nextMultiplicadorAccion = 25
+            }
+            "50" -> {
+                ruletaAccionTextView.text = "50"
+                nextMultiplicadorAccion = 50
+            }
+            "75" -> {
+                ruletaAccionTextView.text = "75"
+                nextMultiplicadorAccion = 75
+            }
+            "100" -> {
+                ruletaAccionTextView.text = "100"
+                nextMultiplicadorAccion = 100
+            }
+            "150" -> {
+                ruletaAccionTextView.text = "150"
+                nextMultiplicadorAccion = 150
+            }
+            "bote" -> {
+                ruletaAccionTextView.text = "¡Bote!"
+                nextMultiplicadorAccion = 0
+                cash+=2000
+
+            }
+            "quiebra" -> {
+                ruletaAccionTextView.text = "¡Quiebra!"
+                nextMultiplicadorAccion = 0
+                cash=0
+            }
+        }
+    }
+
+    private fun setVisibility(isRuletaTirada: Boolean) {
+        // Cambiar la visibilidad de los elementos de la pantalla
+        if (isRuletaTirada) {
+            tirarRuletaButton.visibility = View.INVISIBLE
+            ruletaAccionTextView.visibility = View.VISIBLE
+            setLettersOnClickListeners()
+        } else {
+            tirarRuletaButton.visibility = View.VISIBLE
+            ruletaAccionTextView.visibility = View.GONE
+            hideLettersOnClickListeners()
+        }
+    }
+
+    private fun setLettersOnClickListeners() {
         // Settear el onClickListener de las letras
         lettersLayout.children.forEach {
                 letterView ->
@@ -103,40 +264,13 @@ class RuletaDaSorteGameActivity : AppCompatActivity() {
         }
     }
 
-    private fun playLetter(letterGuessed: Char) {
-        // Mapa de vocales sin tilde a vocales con tilde
-        val accentMap = mapOf(
-            'A' to listOf('Á'),
-            'E' to listOf('É'),
-            'I' to listOf('Í'),
-            'O' to listOf('Ó'),
-            'U' to listOf('Ú'),
-        )
-
-        // Revelar la letra jugada
-        for (letter in letterViews) {
-            if (letter.text == letterGuessed.toString()) {
-                letter.setTextColor(ContextCompat.getColor(this@RuletaDaSorteGameActivity, R.color.black))
-            }
-
-            // Si la letra jugada es una vocal, revelar también las vocales con tilde correspondientes
-            if (accentMap.containsKey(letterGuessed)) {
-                for (accentedLetter in accentMap[letterGuessed]!!) {
-                    if (letter.text == accentedLetter.toString()) {
-                        letter.setTextColor(ContextCompat.getColor(this@RuletaDaSorteGameActivity, R.color.black))
-                    }
-                }
+    private fun hideLettersOnClickListeners() {
+        // Ocultar el onClickListener de las letras
+        lettersLayout.children.forEach {
+                letterView ->
+            if (letterView is TextView) {
+                letterView.setOnClickListener(null)
             }
         }
-    }
-
-    private fun isGameWon(): Boolean {
-        // Verificar si el juego ha sido ganado
-        for (letter in letterViews) {
-            if (letter.currentTextColor == ContextCompat.getColor(this@RuletaDaSorteGameActivity, R.color.canela)) {
-                return false
-            }
-        }
-        return true
     }
 }
