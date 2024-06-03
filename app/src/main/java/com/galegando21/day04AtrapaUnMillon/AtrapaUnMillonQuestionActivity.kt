8f09ae9e68e253dd7389ff4c9447f57ba8338e82
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-06-03 13:10:08 +0200

    Atrapa 1 millón - funcionalidade de arrastrar diñeiro

--- app/src/main/java/com/galegando21/day04AtrapaUnMillon/AtrapaUnMillonQuestionActivity.kt
+++ app/src/main/java/com/galegando21/day04AtrapaUnMillon/AtrapaUnMillonQuestionActivity.kt
@@ -1,14 +1,16 @@
 package com.galegando21.day04AtrapaUnMillon
 
+import android.content.ClipData
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.view.DragEvent
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -18,12 +20,11 @@ import com.galegando21.R
 import com.galegando21.model.QuestionAtrapaUnMillon
 import com.galegando21.utils.AtrapaUnMillonConstants
 import com.galegando21.utils.AtrapaUnMillonConstants.getAtrapaUnMillonQuestions
-import com.galegando21.utils.setBanner
 import com.galegando21.utils.setOnBackPressed
-import kotlin.random.Random
 
 class AtrapaUnMillonQuestionActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var textViewCash : TextView
+    private lateinit var atrapa1MillonImageView: ImageView
     private lateinit var progressBar: ProgressBar
     private lateinit var textViewProgress: TextView
     private lateinit var textViewQuestion: TextView
@@ -38,10 +39,10 @@ class AtrapaUnMillonQuestionActivity : AppCompatActivity(), View.OnClickListener
     private lateinit var textViewOption3: TextView
     private lateinit var textViewOption4: TextView
 
-    private lateinit var editTextOption1 : EditText
-    private lateinit var editTextOption2 : EditText
-    private lateinit var editTextOption3 : EditText
-    private lateinit var editTextOption4 : EditText
+    private lateinit var quantityOption1 : TextView
+    private lateinit var quantityOption2 : TextView
+    private lateinit var quantityOption3 : TextView
+    private lateinit var quantityOption4 : TextView
 
     private lateinit var checkButton: Button
 
@@ -58,6 +59,7 @@ class AtrapaUnMillonQuestionActivity : AppCompatActivity(), View.OnClickListener
         setContentView(R.layout.activity_atrapa_un_millon_question)
 
         textViewCash = findViewById(R.id.cash_tv)
+        atrapa1MillonImageView = findViewById(R.id.atrapa_un_millon_image_view)
         progressBar = findViewById(R.id.atrapa_un_millon_progress_bar)
         textViewProgress = findViewById(R.id.atrapa_un_millon_text_view_progress)
         textViewQuestion = findViewById(R.id.atrapa_un_millon_question_text_view)
@@ -69,19 +71,72 @@ class AtrapaUnMillonQuestionActivity : AppCompatActivity(), View.OnClickListener
         textViewOption2 = findViewById(R.id.atrapa_un_millon_text_view_option2)
         textViewOption3 = findViewById(R.id.atrapa_un_millon_text_view_option3)
         textViewOption4 = findViewById(R.id.atrapa_un_millon_text_view_option4)
-        editTextOption1 = findViewById(R.id.atrapa_un_millon_edittext_option1)
-        editTextOption2 = findViewById(R.id.atrapa_un_millon_edittext_option2)
-        editTextOption3 = findViewById(R.id.atrapa_un_millon_edittext_option3)
-        editTextOption4 = findViewById(R.id.atrapa_un_millon_edittext_option4)
+        quantityOption1 = findViewById(R.id.atrapa_un_millon_quantity_text_option1)
+        quantityOption2 = findViewById(R.id.atrapa_un_millon_quantity_text_option2)
+        quantityOption3 = findViewById(R.id.atrapa_un_millon_quantity_text_option3)
+        quantityOption4 = findViewById(R.id.atrapa_un_millon_quantity_text_option4)
         checkButton = findViewById(R.id.atrapa_un_millon_btn_check)
 
-        setBanner(this, R.string.atrapa_un_millon)
+        val dragMoney = View.OnLongClickListener { v ->
+            val data = ClipData.newPlainText("", "")
+            val shadowBuilder = View.DragShadowBuilder(v)
+            v.startDragAndDrop(data, shadowBuilder, v, 0)
+            true
+        }
 
-        //only accept numbers in editText
-        editTextOption1.inputType=InputType.TYPE_CLASS_NUMBER
-        editTextOption2.inputType=InputType.TYPE_CLASS_NUMBER
-        editTextOption3.inputType=InputType.TYPE_CLASS_NUMBER
-        editTextOption4.inputType=InputType.TYPE_CLASS_NUMBER
+        atrapa1MillonImageView.setOnLongClickListener(dragMoney)
+
+        val relocateMoney = View.OnLongClickListener { v ->
+            val data = ClipData.newPlainText("", "")
+            val shadowBuilder = View.DragShadowBuilder(v)
+            if (v is LinearLayout) {
+                val droppedText = when (v.id) {
+                    R.id.atrapa_un_millon_linear_layout_option1 -> quantityOption1
+                    R.id.atrapa_un_millon_linear_layout_option2 -> quantityOption2
+                    R.id.atrapa_un_millon_linear_layout_option3 -> quantityOption3
+                    R.id.atrapa_un_millon_linear_layout_option4 -> quantityOption4
+                    else -> return@OnLongClickListener true
+                }
+                val currentAmount = droppedText.text.toString().toIntOrNull() ?: 0
+                if (currentAmount > 0) {
+                    droppedText.text = (currentAmount - 100000).toString()
+                    v.startDragAndDrop(data, shadowBuilder, v, 0)
+                    true
+                } else {
+                    false
+                }
+            }
+            true
+        }
+
+        linearLayoutOption1.setOnLongClickListener(relocateMoney)
+        linearLayoutOption2.setOnLongClickListener(relocateMoney)
+        linearLayoutOption3.setOnLongClickListener(relocateMoney)
+        linearLayoutOption4.setOnLongClickListener(relocateMoney)
+
+        val dragListener = View.OnDragListener { v, event ->
+            when (event.action) {
+                DragEvent.ACTION_DROP -> {
+                    if (v is LinearLayout) {
+                        val droppedText = when (v.id) {
+                            R.id.atrapa_un_millon_linear_layout_option1 -> quantityOption1
+                            R.id.atrapa_un_millon_linear_layout_option2 -> quantityOption2
+                            R.id.atrapa_un_millon_linear_layout_option3 -> quantityOption3
+                            R.id.atrapa_un_millon_linear_layout_option4 -> quantityOption4
+                            else -> return@OnDragListener true
+                        }
+                        val currentAmount = droppedText.text.toString().toIntOrNull() ?: 0
+                        droppedText.text = (currentAmount + 100000).toString()
+                    }
+                }
+            }
+            true
+        }
+
+        linearLayoutOption1.setOnDragListener(dragListener)
+        linearLayoutOption2.setOnDragListener(dragListener)
+        linearLayoutOption3.setOnDragListener(dragListener)
+        linearLayoutOption4.setOnDragListener(dragListener)
 
         checkButton.setOnClickListener(this)
 
@@ -110,7 +165,7 @@ class AtrapaUnMillonQuestionActivity : AppCompatActivity(), View.OnClickListener
     private fun showNextQuestion() {
 
         if (questionsCounter < 8 && cash!=0) {
-            checkButton.text = "Check"
+            checkButton.text = "Comprobar"
 
             resetOptions()
             val question = questionsList[questionsCounter]
@@ -160,13 +215,13 @@ class AtrapaUnMillonQuestionActivity : AppCompatActivity(), View.OnClickListener
             )
         }
 
-        val editTextOptions = mutableListOf<EditText>()
-        editTextOptions.add(editTextOption1)
-        editTextOptions.add(editTextOption2)
-        editTextOptions.add(editTextOption3)
-        editTextOptions.add(editTextOption4)
-        for (option in editTextOptions) {
-            option.text.clear()
+        val quantityTextOptions = mutableListOf<TextView>()
+        quantityTextOptions.add(quantityOption1)
+        quantityTextOptions.add(quantityOption2)
+        quantityTextOptions.add(quantityOption3)
+        quantityTextOptions.add(quantityOption4)
+        for (option in quantityTextOptions) {
+            option.text = "0"
         }
     }
 
@@ -179,31 +234,31 @@ class AtrapaUnMillonQuestionActivity : AppCompatActivity(), View.OnClickListener
         val solution = currentQuestion.correctAnswer
         when (solution) {
             1 -> {
-                if (editTextOption1.text.isEmpty()) {
+                if (quantityOption1.text.isEmpty()) {
                     cash = 0
                 } else {
-                    cash = editTextOption1.text.toString().toInt()
+                    cash = quantityOption1.text.toString().toInt()
                 }
             }
             2 -> {
-                if (editTextOption2.text.isEmpty()) {
+                if (quantityOption2.text.isEmpty()) {
                     cash = 0
                 } else {
-                    cash = editTextOption2.text.toString().toInt()
+                    cash = quantityOption2.text.toString().toInt()
                 }
             }
             3 -> {
-                if (editTextOption3.text.isEmpty()) {
+                if (quantityOption3.text.isEmpty()) {
                     cash = 0
                 } else {
-                    cash = editTextOption3.text.toString().toInt()
+                    cash = quantityOption3.text.toString().toInt()
                 }
             }
             4 -> {
-                if (editTextOption4.text.isEmpty()) {
+                if (quantityOption4.text.isEmpty()) {
                     cash = 0
                 } else {
-                    cash = editTextOption4.text.toString().toInt()
+                    cash = quantityOption4.text.toString().toInt()
                 }
             }
         }
@@ -216,20 +271,20 @@ class AtrapaUnMillonQuestionActivity : AppCompatActivity(), View.OnClickListener
         when (view?.id) {
             R.id.atrapa_un_millon_btn_check -> {
                 var valueOption1 = 0
-                if (editTextOption1.text.isNotEmpty()) {
-                    valueOption1 = editTextOption1.text.toString().toInt()
+                if (quantityOption1.text.isNotEmpty()) {
+                    valueOption1 = quantityOption1.text.toString().toInt()
                 }
                 var valueOption2 = 0
-                if (editTextOption2.text.isNotEmpty()) {
-                    valueOption2 = editTextOption2.text.toString().toInt()
+                if (quantityOption2.text.isNotEmpty()) {
+                    valueOption2 = quantityOption2.text.toString().toInt()
                 }
                 var valueOption3 = 0
-                if (editTextOption3.text.isNotEmpty()) {
-                    valueOption3 = editTextOption3.text.toString().toInt()
+                if (quantityOption3.text.isNotEmpty()) {
+                    valueOption3 = quantityOption3.text.toString().toInt()
                 }
                 var valueOption4 = 0
-                if (editTextOption4.text.isNotEmpty()) {
-                    valueOption4 = editTextOption4.text.toString().toInt()
+                if (quantityOption4.text.isNotEmpty()) {
+                    valueOption4 = quantityOption4.text.toString().toInt()
                 }
                 val sum = valueOption1+valueOption2+valueOption3+valueOption4
                 if (!answered) {
