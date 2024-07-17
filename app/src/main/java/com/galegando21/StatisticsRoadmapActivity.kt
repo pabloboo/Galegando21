6d7126545c77e372fb72df02c7351f4a07e34f3f
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-07-17 20:34:48 +0200

    Redeseño e adición de insignias

--- app/src/main/java/com/galegando21/StatisticsRoadmapActivity.kt
+++ app/src/main/java/com/galegando21/StatisticsRoadmapActivity.kt
@@ -1,9 +1,12 @@
 package com.galegando21
 
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
@@ -21,15 +24,60 @@ class StatisticsRoadmapActivity : AppCompatActivity() {
     private lateinit var maxDayExperienceStatistics: TextView
     private lateinit var maxDayExperienceStatisticsDate: TextView
 
-    private lateinit var insigniaRacha7 : LinearLayout
-    private lateinit var insigniaRacha21 : LinearLayout
-    private lateinit var insigniaRacha50 : LinearLayout
-    private lateinit var insigniaRacha100 : LinearLayout
-    private lateinit var insigniaPasagalegoComplete : LinearLayout
-    private lateinit var insigniaXogoPalabrasComplete : LinearLayout
-    private lateinit var insigniaAtrapaUnMillonComplete : LinearLayout
-    private lateinit var insigniaAdivinhaAnoFotoComplete : LinearLayout
-    private lateinit var insigniaAdivinhaEscudoComplete: LinearLayout
+    private lateinit var insigniaRacha7 : ImageView
+    private lateinit var lockInsigniaRacha7 : ImageView
+    private lateinit var insigniaRacha21 : ImageView
+    private lateinit var lockInsigniaRacha21 : ImageView
+    private lateinit var insigniaRacha50 : ImageView
+    private lateinit var lockInsigniaRacha50 : ImageView
+    private lateinit var insigniaRacha100 : ImageView
+    private lateinit var lockInsigniaRacha100 : ImageView
+    private lateinit var insigniaPasagalego : ImageView
+    private lateinit var lockInsigniaPasagalego : ImageView
+    private lateinit var insigniaOndeEstan: ImageView
+    private lateinit var lockInsigniaOndeEstan: ImageView
+    private lateinit var insigniaXogoPalabras : ImageView
+    private lateinit var lockInsigniaXogoPalabras : ImageView
+    private lateinit var insigniaAtrapaUnMillon : ImageView
+    private lateinit var lockInsigniaAtrapaUnMillon : ImageView
+    private lateinit var insigniaAdivinhaAnoFoto : ImageView
+    private lateinit var lockInsigniaAdivinhaAnoFoto : ImageView
+    private lateinit var insigniaSopaLetras: ImageView
+    private lateinit var lockInsigniaSopaLetras: ImageView
+    private lateinit var insigniaAgoraCaigo: ImageView
+    private lateinit var lockInsigniaAgoraCaigo: ImageView
+    private lateinit var insigniaConexions: ImageView
+    private lateinit var lockInsigniaConexions: ImageView
+    private lateinit var insigniaRuletaDaSorte: ImageView
+    private lateinit var lockInsigniaRuletaDaSorte: ImageView
+    private lateinit var insigniaExplosionPalabras: ImageView
+    private lateinit var lockInsigniaExplosionPalabras: ImageView
+    private lateinit var insigniaAtrapameSePodes: ImageView
+    private lateinit var lockInsigniaAtrapameSePodes: ImageView
+    private lateinit var insigniaAdivinhaPersonaxe: ImageView
+    private lateinit var lockInsigniaAdivinhaPersonaxe: ImageView
+    private lateinit var insigniaVerdadeOuMentira: ImageView
+    private lateinit var lockInsigniaVerdadeOuMentira: ImageView
+    private lateinit var insigniaAdivinhaEscudo: ImageView
+    private lateinit var lockInsigniaAdivinhaEscudo: ImageView
+    private lateinit var insigniaWordle: ImageView
+    private lateinit var lockInsigniaWordle: ImageView
+    private lateinit var insigniaAnagramas: ImageView
+    private lateinit var lockInsigniaAnagramas: ImageView
+    private lateinit var insigniaProbaVelocidade: ImageView
+    private lateinit var lockInsigniaProbaVelocidade: ImageView
+    private lateinit var insigniaPalabrasEncadeadas: ImageView
+    private lateinit var lockInsigniaPalabrasEncadeadas: ImageView
+    private lateinit var insigniaAforcado: ImageView
+    private lateinit var lockInsigniaAforcado: ImageView
+    private lateinit var insigniaExperiencia10k: ImageView
+    private lateinit var lockInsigniaExperiencia10k: ImageView
+    private lateinit var insigniaExperiencia50k: ImageView
+    private lateinit var lockInsigniaExperiencia50k: ImageView
+    private lateinit var insigniaExperiencia100k: ImageView
+    private lateinit var lockInsigniaExperiencia100k: ImageView
+    private lateinit var insigniaExperiencia1m: ImageView
+    private lateinit var lockInsigniaExperiencia1m: ImageView
 
     private lateinit var correctAnswersPasagalegoDiccionarioFacil : TextView
     private lateinit var errorAnswersPasagalegoDiccionarioFacil : TextView
@@ -88,14 +136,59 @@ class StatisticsRoadmapActivity : AppCompatActivity() {
         maxDayExperienceStatisticsDate = findViewById(R.id.max_day_experience_statistics_day)
 
         insigniaRacha7 = findViewById(R.id.insignia_racha_7)
+        lockInsigniaRacha7 = findViewById(R.id.lock_insignia_racha_7)
         insigniaRacha21 = findViewById(R.id.insignia_racha_21)
+        lockInsigniaRacha21 = findViewById(R.id.lock_insignia_racha_21)
         insigniaRacha50 = findViewById(R.id.insignia_racha_50)
+        lockInsigniaRacha50 = findViewById(R.id.lock_insignia_racha_50)
         insigniaRacha100 = findViewById(R.id.insignia_racha_100)
-        insigniaPasagalegoComplete = findViewById(R.id.insignia_pasagalego_complete)
-        insigniaXogoPalabrasComplete = findViewById(R.id.insignia_xogo_palabras_complete)
-        insigniaAtrapaUnMillonComplete = findViewById(R.id.insignia_atrapa_un_millon_complete)
-        insigniaAdivinhaAnoFotoComplete = findViewById(R.id.insignia_adivinha_ano_foto_complete)
-        insigniaAdivinhaEscudoComplete = findViewById(R.id.insignia_adivinha_escudo_complete)
+        lockInsigniaRacha100 = findViewById(R.id.lock_insignia_racha_100)
+        insigniaPasagalego = findViewById(R.id.insignia_pasagalego)
+        lockInsigniaPasagalego = findViewById(R.id.lock_insignia_pasagalego)
+        insigniaOndeEstan = findViewById(R.id.insignia_onde_estan)
+        lockInsigniaOndeEstan = findViewById(R.id.lock_insignia_onde_estan)
+        insigniaXogoPalabras = findViewById(R.id.insignia_xogo_palabras)
+        lockInsigniaXogoPalabras = findViewById(R.id.lock_insignia_xogo_palabras)
+        insigniaAtrapaUnMillon = findViewById(R.id.insignia_atrapa_1m)
+        lockInsigniaAtrapaUnMillon = findViewById(R.id.lock_insignia_atrapa_1m)
+        insigniaAdivinhaAnoFoto = findViewById(R.id.insignia_adivinhar_ano_foto)
+        lockInsigniaAdivinhaAnoFoto = findViewById(R.id.lock_insignia_adivinhar_ano_foto)
+        insigniaSopaLetras = findViewById(R.id.insignia_sopa_letras)
+        lockInsigniaSopaLetras = findViewById(R.id.lock_insignia_sopa_letras)
+        insigniaAgoraCaigo = findViewById(R.id.insignia_agora_caigo)
+        lockInsigniaAgoraCaigo = findViewById(R.id.lock_insignia_agora_caigo)
+        insigniaConexions = findViewById(R.id.insignia_conexions)
+        lockInsigniaConexions = findViewById(R.id.lock_insignia_conexions)
+        insigniaRuletaDaSorte = findViewById(R.id.insignia_ruleta_sorte)
+        lockInsigniaRuletaDaSorte = findViewById(R.id.lock_insignia_ruleta_sorte)
+        insigniaExplosionPalabras = findViewById(R.id.insignia_explosion_palabras)
+        lockInsigniaExplosionPalabras = findViewById(R.id.lock_insignia_explosion_palabras)
+        insigniaAtrapameSePodes = findViewById(R.id.insignia_atrapame_se_podes)
+        lockInsigniaAtrapameSePodes = findViewById(R.id.lock_insignia_atrapame_se_podes)
+        insigniaAdivinhaPersonaxe = findViewById(R.id.insignia_adivinha_personaxe)
+        lockInsigniaAdivinhaPersonaxe = findViewById(R.id.lock_insignia_adivinha_personaxe)
+        insigniaVerdadeOuMentira = findViewById(R.id.insignia_verdade_ou_mentira)
+        lockInsigniaVerdadeOuMentira = findViewById(R.id.lock_insignia_verdade_ou_mentira)
+        insigniaAdivinhaEscudo = findViewById(R.id.insignia_adivinhar_escudo)
+        lockInsigniaAdivinhaEscudo = findViewById(R.id.lock_insignia_adivinhar_escudo)
+        insigniaWordle = findViewById(R.id.insignia_wordle)
+        lockInsigniaWordle = findViewById(R.id.lock_insignia_wordle)
+        insigniaAnagramas = findViewById(R.id.insignia_anagramas)
+        lockInsigniaAnagramas = findViewById(R.id.lock_insignia_anagramas)
+        insigniaProbaVelocidade = findViewById(R.id.insignia_proba_velocidade)
+        lockInsigniaProbaVelocidade = findViewById(R.id.lock_insignia_proba_velocidade)
+        insigniaPalabrasEncadeadas = findViewById(R.id.insignia_palabras_encadeadas)
+        lockInsigniaPalabrasEncadeadas = findViewById(R.id.lock_insignia_palabras_encadeadas)
+        insigniaAforcado = findViewById(R.id.insignia_aforcado)
+        lockInsigniaAforcado = findViewById(R.id.lock_insignia_aforcado)
+        insigniaExperiencia10k = findViewById(R.id.insignia_experiencia_10k)
+        lockInsigniaExperiencia10k = findViewById(R.id.lock_insignia_experiencia_10k)
+        insigniaExperiencia50k = findViewById(R.id.insignia_experiencia_50k)
+        lockInsigniaExperiencia50k = findViewById(R.id.lock_insignia_experiencia_50k)
+        insigniaExperiencia100k = findViewById(R.id.insignia_experiencia_100k)
+        lockInsigniaExperiencia100k = findViewById(R.id.lock_insignia_experiencia_100k)
+        insigniaExperiencia1m = findViewById(R.id.insignia_experiencia_1m)
+        lockInsigniaExperiencia1m = findViewById(R.id.lock_insignia_experiencia_1m)
 
         correctAnswersPasagalegoDiccionarioFacil = findViewById(R.id.correct_answers_pasagalego_statistics_diccionario_facil)
         errorAnswersPasagalegoDiccionarioFacil = findViewById(R.id.error_answers_pasagalego_statistics_diccionario_facil)
@@ -212,38 +305,141 @@ class StatisticsRoadmapActivity : AppCompatActivity() {
     private fun setInsignias() {
         val sharedPreferences = getSharedPreferences(SharedPreferencesKeys.STATISTICS, MODE_PRIVATE)
 
+        val colorMatrix = ColorMatrix()
+        colorMatrix.setSaturation(0f) // Escala de grises
+        val filter = ColorMatrixColorFilter(colorMatrix)
+
         val longestStreak = sharedPreferences.getInt(SharedPreferencesKeys.LONGEST_STREAK, 0).toString().toInt()
-        if (longestStreak >= 7) {
-            insigniaRacha7.visibility = View.VISIBLE
+        if (longestStreak <= 7) {
+            insigniaRacha7.colorFilter = filter
+            lockInsigniaRacha7.visibility = View.VISIBLE
         }
-        if (longestStreak >= 21) {
-            insigniaRacha21.visibility = View.VISIBLE
+        if (longestStreak <= 21) {
+            insigniaRacha21.colorFilter = filter
+            lockInsigniaRacha21.visibility = View.VISIBLE
         }
-        if (longestStreak >= 50) {
-            insigniaRacha50.visibility = View.VISIBLE
+        if (longestStreak <= 50) {
+            insigniaRacha50.colorFilter = filter
+            lockInsigniaRacha50.visibility = View.VISIBLE
         }
-        if (longestStreak >= 100) {
-            insigniaRacha100.visibility = View.VISIBLE
-        }
-
-        if (sharedPreferences.getInt(SharedPreferencesKeys.PASAGALEGO_CORRECT_ANSWERS_DICTIONARY, 0).toString().toInt() == 23) {
-            insigniaPasagalegoComplete.visibility = View.VISIBLE
-        }
-
-        if (sharedPreferences.getFloat(SharedPreferencesKeys.XOGO_PALABRAS_MAX_SCORE, 0F).toString().toFloat() == 100F) {
-            insigniaXogoPalabrasComplete.visibility = View.VISIBLE
+        if (longestStreak <= 100) {
+            insigniaRacha100.colorFilter = filter
+            lockInsigniaRacha100.visibility = View.VISIBLE
         }
 
-        if (sharedPreferences.getInt(SharedPreferencesKeys.ATRAPA_UN_MILLON_MAX_CASH, 0).toString().toInt() == 1000000) {
-            insigniaAtrapaUnMillonComplete.visibility = View.VISIBLE
+        if (sharedPreferences.getInt(SharedPreferencesKeys.PASAGALEGO_CORRECT_ANSWERS_DICTIONARY, 0).toString().toInt() != 23) {
+            insigniaPasagalego.colorFilter = filter
+            lockInsigniaPasagalego.visibility = View.VISIBLE
         }
 
-        if (sharedPreferences.getInt(SharedPreferencesKeys.ADIVINHA_ANO_FOTO_MAX_SCORE, 0).toString().toInt() == 50) {
-            insigniaAdivinhaAnoFotoComplete.visibility = View.VISIBLE
+        if (sharedPreferences.getInt(SharedPreferencesKeys.ONDE_ESTAN_MIN_TIME, 0).toString().toInt() > 20 || sharedPreferences.getInt(SharedPreferencesKeys.ONDE_ESTAN_MIN_TIME, 0).toString().toInt() == 0) {
+            insigniaOndeEstan.colorFilter = filter
+            lockInsigniaOndeEstan.visibility = View.VISIBLE
         }
 
-        if (sharedPreferences.getInt(SharedPreferencesKeys.ADIVINHA_ESCUDO_MAX_SCORE, 0).toString().toInt() == 10) {
-            insigniaAdivinhaEscudoComplete.visibility = View.VISIBLE
+        if (sharedPreferences.getFloat(SharedPreferencesKeys.XOGO_PALABRAS_MAX_SCORE, 0F).toString().toFloat() < 100F) {
+            insigniaXogoPalabras.colorFilter = filter
+            lockInsigniaXogoPalabras.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.ATRAPA_UN_MILLON_MAX_CASH, 0).toString().toInt() < 1000000) {
+            insigniaAtrapaUnMillon.colorFilter = filter
+            lockInsigniaAtrapaUnMillon.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.ADIVINHA_ANO_FOTO_MAX_SCORE, 0).toString().toInt() < 50) {
+            insigniaAdivinhaAnoFoto.colorFilter = filter
+            lockInsigniaAdivinhaAnoFoto.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.SOPA_LETRAS_MAX_SCORE, 0).toString().toInt() < 500) {
+            insigniaSopaLetras.colorFilter = filter
+            lockInsigniaSopaLetras.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.AGORA_CAIGO_MAX_SCORE, 0).toString().toInt() < 50) {
+            insigniaAgoraCaigo.colorFilter = filter
+            lockInsigniaAgoraCaigo.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.CONEXIONS_MAX_SCORE, 0).toString().toInt() < 5) {
+            insigniaConexions.colorFilter = filter
+            lockInsigniaConexions.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.RULETA_DA_SORTE_MAX_CASH, 0).toString().toInt() < 4000 || sharedPreferences.getInt(SharedPreferencesKeys.RULETA_DA_SORTE_MAX_CASH, 0).toString().toInt() == 0) {
+            insigniaRuletaDaSorte.colorFilter = filter
+            lockInsigniaRuletaDaSorte.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.EXPLOSION_PALABRAS_MAX_SCORE_DIFICULT, 0).toString().toInt() < 100) {
+            insigniaExplosionPalabras.colorFilter = filter
+            lockInsigniaExplosionPalabras.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.ATRAPAME_SE_PODES_QUESTIONS_NEEDED, 0).toString().toInt() < 5) {
+            insigniaAtrapameSePodes.colorFilter = filter
+            lockInsigniaAtrapameSePodes.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.ADIVINHA_PERSONAXE_MAX_SCORE, 0).toString().toInt() > 2 || sharedPreferences.getInt(SharedPreferencesKeys.ADIVINHA_PERSONAXE_MAX_SCORE, 0).toString().toInt() == 0) {
+            insigniaAdivinhaPersonaxe.colorFilter = filter
+            lockInsigniaAdivinhaPersonaxe.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.VERDADE_OU_MENTIRA_MAX_SCORE, 0).toString().toInt() < 30) {
+            insigniaVerdadeOuMentira.colorFilter = filter
+            lockInsigniaVerdadeOuMentira.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.ADIVINHA_ESCUDO_MAX_SCORE, 0).toString().toInt() < 10) {
+            insigniaAdivinhaEscudo.colorFilter = filter
+            lockInsigniaAdivinhaEscudo.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.WORDLE_MAX_STREAK, 0).toString().toInt() < 10) {
+            insigniaWordle.colorFilter = filter
+            lockInsigniaWordle.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.ANAGRAMAS_MAX_SCORE_DIFICULT, 0).toString().toInt() < 20) {
+            insigniaAnagramas.colorFilter = filter
+            lockInsigniaAnagramas.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.PROBA_VELOCIDADE_MIN_TIME, 0).toString().toInt() < 40 || sharedPreferences.getInt(SharedPreferencesKeys.PROBA_VELOCIDADE_MIN_TIME, 0).toString().toInt() == 0) {
+            insigniaProbaVelocidade.colorFilter = filter
+            lockInsigniaProbaVelocidade.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.PALABRAS_ENCADEADAS_MAX_SCORE, 0).toString().toInt() < 20) {
+            insigniaPalabrasEncadeadas.colorFilter = filter
+            lockInsigniaPalabrasEncadeadas.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.AFORCADO_MAX_STREAK_DIFICULT, 0).toString().toInt() < 10) {
+            insigniaAforcado.colorFilter = filter
+            lockInsigniaAforcado.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.EXPERIENCE_POINTS, 0).toString().toInt() < 10000) {
+            insigniaExperiencia10k.colorFilter = filter
+            lockInsigniaExperiencia10k.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.EXPERIENCE_POINTS, 0).toString().toInt() < 50000) {
+            insigniaExperiencia50k.colorFilter = filter
+            lockInsigniaExperiencia50k.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.EXPERIENCE_POINTS, 0).toString().toInt() < 100000) {
+            insigniaExperiencia100k.colorFilter = filter
+            lockInsigniaExperiencia100k.visibility = View.VISIBLE
+        }
+
+        if (sharedPreferences.getInt(SharedPreferencesKeys.EXPERIENCE_POINTS, 0).toString().toInt() < 1000000) {
+            insigniaExperiencia1m.colorFilter = filter
+            lockInsigniaExperiencia1m.visibility = View.VISIBLE
         }
 
     }
